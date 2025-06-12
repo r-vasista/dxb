eda60ba9498345d2dc5d69398fb3d09e9df11b33
Vasista <vasista.rachaputi@gmail.com>
2025-06-12 11:25:02 +0530

    updated url

--- dxb/settings.py
+++ dxb/settings.py
@@ -196,4 +196,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_DOMAIL_URL = os.environ.get('EMAIL_DOMAIL_URL', '')
 
-FRONTEND_URL = 'http://127.0.0.1:8000/'
+FRONTEND_URL = 'http://127.0.0.1:8000'
