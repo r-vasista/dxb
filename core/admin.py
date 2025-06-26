1fff22e91af1c04cd3e21ed31057152afdec91c8
vasista <vasista.rachaputi@gmail.com>
2025-06-26 15:30:38 +0530

    create dynamic email flow

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from core.models import EmailConfiguration, EmailTemplate
 
-# Register your models here.
+@admin.register(EmailTemplate)
+class EmailTemplateAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'subject', 'title', 'main_content']
+    search_fields = ['id', 'name', 'subject', 'title', 'main_content']
+    list_filter = ['id', 'name', 'subject', 'title', 'main_content']
+
+
+@admin.register(EmailConfiguration)
+class EmailConfigurationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'company_name', 'company_logo_url', 'contact_email']
+    search_fields = ['id', 'company_name', 'company_logo_url', 'contact_email']
+    list_filter = ['id', 'company_name', 'company_logo_url', 'contact_email']
+
