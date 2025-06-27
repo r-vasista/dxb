f2f850c36bcd201d3cd097ad0e277ca691e41293
vasista <vasista.rachaputi@gmail.com>
2025-06-27 17:06:41 +0530

    added post reactions and comments table in admin

--- post/admin.py
+++ post/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from post.models import Post, PostMedia, Hashtag
+from post.models import Post, PostMedia, Hashtag, PostReaction, Comment, CommentLike
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'profile', 'title']
+
 
 @admin.register(PostMedia)
 class PostMediaAdmin(admin.ModelAdmin):
@@ -15,3 +16,24 @@ class HashtagAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
     search_fields = ['id', 'name']
     list_filter = ['id', 'name']
+
+
+@admin.register(PostReaction)
+class PostReactionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'post', 'profile', 'reaction_type']
+    search_fields = ['id', 'post', 'profile', 'reaction_type']
+    list_filter = ['id', 'post', 'profile', 'reaction_type']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'post', 'profile', 'content', 'parent']
+    search_fields =  ['id', 'post', 'profile', 'content', 'parent']
+    list_filter =  ['id', 'post', 'profile', 'content', 'parent']
+
+
+@admin.register(CommentLike)
+class CommentLikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'comment', 'profile']
+    search_fields = ['id', 'comment', 'profile']
+    list_filter = ['id', 'comment', 'profile']
